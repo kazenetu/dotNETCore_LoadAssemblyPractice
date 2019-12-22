572db5b69e09eec362f13920db2f652f6e565a82
kazenetu <rakudafactory@gmail.com>
2019-12-22 23:05:58 +0900

    テストクラス用インターフェースを追加

---
+++ Interface/Interface/ITestClass.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Interface
+{
+  /// <summary>
+  /// テストクラス用インターフェース
+  /// </summary>
+  public interface ITestClass
+  {
+    /// <summary>
+    /// helloworldを返す
+    /// </summary>
+    /// <returns>helloworld文字列</returns>
+    public string HelloWorld();
+
+    /// <summary>
+    /// 文字列から数値型に変換する
+    /// </summary>
+    /// <param name="src">文字列</param>
+    /// <returns>数値</returns>
+    public decimal StringToInt(string src);
+
+    /// <summary>
+    /// 文字列リストを取得する
+    /// </summary>
+    /// <returns>文字列リスト</returns>
+    public List<string> GetList();
+  }
+}
