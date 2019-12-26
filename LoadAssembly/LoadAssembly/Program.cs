ccfac4430d84481c918ce2fa039e4d39e7458852
kazenetu <rakudafactory@gmail.com>
2019-12-26 22:19:35 +0900

    クラスインスタンスのメソッドを呼び出すメソッドを切り出し

--- LoadAssembly/LoadAssembly/Program.cs
+++ LoadAssembly/LoadAssembly/Program.cs
@@ -10,11 +10,25 @@ namespace LoadAssembly
     {
       // 読み込み対象のdllのフルパス設定
       var thisPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-      var assemblyPath = Path.Combine(thisPath,"LoadTarget.dll");
+      var assemblyPath = Path.Combine(thisPath, "LoadTarget.dll");
 
       // dll読み込み
       var alc = new TestAssemblyLoadContext();
       Assembly a = alc.LoadFromAssemblyPath(assemblyPath);
+
+      // クラスインスタンスのメソッド呼び出し
+      callClassMethod(a);
+
+      // dllアセンブリを解放
+      alc.Unload();
+    }
+
+    /// <summary>
+    /// クラスインスタンスのメソッドを呼び出し
+    /// </summary>
+    /// <param name="a">読み込んだアセンブリ</param>
+    static void callClassMethod(Assembly a)
+    {
 
       // 対象クラスのインスタンス生成
       var type = a.GetType("LoadTarget.Class1");
@@ -26,7 +40,6 @@ namespace LoadAssembly
       // メソッド呼び出し(戻り値 string)
       Console.WriteLine($"testMethod call:[{testMethod.Invoke(instance, null)}]");
 
-      alc.Unload();
     }
   }
 }
