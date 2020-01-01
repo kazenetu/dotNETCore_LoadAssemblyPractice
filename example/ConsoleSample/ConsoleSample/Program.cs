84ed58aeedab0f51b7fb30372c961c84796db310
kazenetu <rakudafactory@gmail.com>
2020-01-01 21:28:22 +0900

    コンソールの実装例を追加

---
+++ example/ConsoleSample/ConsoleSample/Program.cs
@@ -0,0 +1,69 @@
+﻿using Interface;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace LoadAssembly
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // 読み込み対象のdllのフルパス設定
+      var thisPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+      var assemblyPath = Path.Combine(thisPath, "LoadTarget.dll");
+
+      // dll読み込み
+      var alc = new TestAssemblyLoadContext();
+      Assembly a = alc.LoadFromAssemblyPath(assemblyPath);
+
+      // クラスインスタンスのメソッド呼び出し
+      callClassMethod(a);
+
+      // インターフェースのメソッドを実行
+      callInterfaceMethod(a);
+
+      // dllアセンブリを解放
+      alc.Unload();
+    }
+
+    /// <summary>
+    /// クラスインスタンスのメソッドを実行
+    /// </summary>
+    /// <param name="a">読み込んだアセンブリ</param>
+    static void callClassMethod(Assembly a)
+    {
+
+      // 対象クラスのインスタンス生成
+      var type = a.GetType("LoadTarget.Class1");
+      var instance = Activator.CreateInstance(type);
+
+      // 呼び出しメソッドを取得
+      var testMethod = type.GetMethod("TestMethod");
+
+      // メソッド呼び出し(戻り値 string)
+      Console.WriteLine($"testMethod call:[{testMethod.Invoke(instance, null)}]");
+
+    }
+
+    /// <summary>
+    /// インターフェースのメソッドを実行
+    /// </summary>
+    /// <param name="a">読み込んだアセンブリ</param>
+    static void callInterfaceMethod(Assembly a)
+    {
+
+      // 対象クラスのインスタンス生成
+      var type = a.GetType("LoadTarget.TestClass");
+      var instance = Activator.CreateInstance(type) as ITestClass;
+
+      // メソッド呼び出し(戻り値 string)
+      Console.WriteLine($"HelloWorld call:[{instance.HelloWorld()}]");
+
+      // リスト系メソッド呼び出し(戻り値 string)
+      Console.WriteLine($"GetList call:[{string.Join(" , ", instance.GetList())}]");
+
+    }
+
+  }
+}
