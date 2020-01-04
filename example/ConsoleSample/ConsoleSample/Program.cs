3918350202dbbbabfa215928e5f73ca4f48063a6
kazenetu <rakudafactory@gmail.com>
2020-01-04 21:43:15 +0900

    インターフェースを継承したクラスのインスタンスを取得するメソッドを作成

--- example/ConsoleSample/ConsoleSample/Program.cs
+++ example/ConsoleSample/ConsoleSample/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.IO;
 using System.Reflection;
+using System.Linq;
 
 namespace LoadAssembly
 {
@@ -52,10 +53,14 @@ namespace LoadAssembly
     /// <param name="a">読み込んだアセンブリ</param>
     static void callInterfaceMethod(Assembly a)
     {
-
-      // 対象クラスのインスタンス生成
-      var type = a.GetType("LoadTarget.TestClass");
-      var instance = Activator.CreateInstance(type) as ITestClass;
+      // 対象クラスのインスタンス取得
+      var instance = GetInterfaceInstance<ITestClass>(a);
+      if (instance is null)
+      {
+        // 取得できない場合は終了
+        Console.WriteLine($"callInterfaceMethod:[noclass:TestClass]");
+        return;
+      }
 
       // メソッド呼び出し(戻り値 string)
       Console.WriteLine($"HelloWorld call:[{instance.HelloWorld()}]");
@@ -65,5 +70,34 @@ namespace LoadAssembly
 
     }
 
+    /// <summary>
+    /// インターフェースインスタンス取得
+    /// </summary>
+    /// <typeparam name="T">インターフェース</typeparam>
+    /// <param name="assembly">読み込んだアセンブリ</param>
+    /// <returns>インターフェースインスタンスまたはnull</returns>
+    static T GetInterfaceInstance<T>(Assembly assembly) where T : class
+    {
+      // 対象インターフェース名取得
+      var typeName = typeof(T).FullName;
+
+      // アセンブリからインターフェースを継承したクラスのTypeリストを取得
+      var targets = assembly.GetTypes().Where(typeItem => typeItem.IsClass && typeItem.GetInterface(typeName) != null);
+      if (!targets.Any())
+      {
+        return null;
+      }
+
+      // 最初のクラスTypeを取得
+      var type = assembly.GetType(targets.First().FullName);
+      if (type is null)
+      {
+        return null;
+
+      }
+
+      // インスタンスを返す
+      return Activator.CreateInstance(type) as T;
+    }
   }
 }
