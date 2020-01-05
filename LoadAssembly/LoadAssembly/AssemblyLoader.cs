0934d938656cb3fe344dc2827e9f01fca5b24f92
kazenetu <rakudafactory@gmail.com>
2020-01-05 09:37:19 +0900

    読込アセンブリ管理クラスを追加

---
+++ LoadAssembly/LoadAssembly/AssemblyLoader.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LoadAssembly
+{
+  /// <summary>
+  /// アセンブリの読み込みを管理するクラス
+  /// </summary>
+  public class AssemblyLoader
+  {
+    /// <summary>
+    /// 読み込み済みアセンブリType
+    /// </summary>
+    protected List<Type> assemblyTypes = new List<Type>();
+
+    /// <summary>
+    /// アセンブリのロード
+    /// </summary>
+    /// <param name="filePath">ファイルの絶対パス</param>
+    public void Load(string filePath)
+    {
+      // アセンブリの読み込み
+      var alc = new TestAssemblyLoadContext();
+      var assembly = alc.LoadFromAssemblyPath(filePath);
+
+      // リストに追加
+      assemblyTypes.AddRange(assembly.GetTypes());
+    }
+
+    /// <summary>
+    /// インターフェースインスタンス取得
+    /// </summary>
+    /// <typeparam name="T">インターフェース</typeparam>
+    /// <returns>インターフェースインスタンスまたはnull</returns>
+    public T GetInterfaceInstance<T>() where T : class
+    {
+      // 対象インターフェース名取得
+      var typeName = typeof(T).FullName;
+
+      // アセンブリからインターフェースを継承したクラスのTypeリストを取得
+      var targets = assemblyTypes.Where(typeItem => typeItem.IsClass && typeItem.GetInterface(typeName) != null);
+      if (!targets.Any())
+      {
+        return null;
+      }
+
+      // 最初のクラスTypeを取得
+      var type = targets.First();
+      if (type is null)
+      {
+        return null;
+
+      }
+
+      // インスタンスを返す
+      return Activator.CreateInstance(type) as T;
+    }
+
+  }
+}
