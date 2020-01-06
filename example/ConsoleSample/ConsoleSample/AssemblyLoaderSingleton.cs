2132169c3198ace9816e912485c2c1913bcca52d
kazenetu <rakudafactory@gmail.com>
2020-01-06 21:26:27 +0900

    コンソール用シングルトンのアセンブリ管理クラスを追加

---
+++ example/ConsoleSample/ConsoleSample/AssemblyLoaderSingleton.cs
@@ -0,0 +1,33 @@
+﻿using LoadAssembly;
+
+namespace ConsoleSample
+{
+  /// <summary>
+  /// コンソール用アセンブリローダー(シングルトン)
+  /// </summary>
+  public class AssemblyLoaderSingleton : AssemblyLoader
+  {
+    /// <summary>
+    /// インスタンス
+    /// </summary>
+    private static AssemblyLoaderSingleton instance = new AssemblyLoaderSingleton();
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <remarks>シングルトンのためprivate</remarks>
+    private AssemblyLoaderSingleton()
+    {
+
+    }
+
+    /// <summary>
+    /// インスタンス取得
+    /// </summary>
+    /// <returns>インスタンス</returns>
+    public static AssemblyLoaderSingleton GetInstance()
+    {
+      return instance;
+    }
+  }
+}
