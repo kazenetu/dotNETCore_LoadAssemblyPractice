d690292478fa7c381b889c61df70536d22314c5e
kazenetu <rakudafactory@gmail.com>
2020-01-07 20:20:21 +0900

    アセンブリ読み込み管理インターフェイスを追加

---
+++ LoadAssembly/LoadAssembly/IAssemblyLoader.cs
@@ -0,0 +1,21 @@
+﻿namespace LoadAssembly
+{
+  /// <summary>
+  /// アセンブリ読み込み管理インターフェイス
+  /// </summary>
+  public interface IAssemblyLoader
+  {
+    /// <summary>
+    /// アセンブリのロード
+    /// </summary>
+    /// <param name="filePath">ファイルの絶対パス</param>
+    void Load(string filePath);
+
+    /// <summary>
+    /// インターフェースインスタンス取得
+    /// </summary>
+    /// <typeparam name="T">インターフェース</typeparam>
+    /// <returns>インターフェースインスタンスまたはnull</returns>
+    public T GetInterfaceInstance<T>() where T : class;
+  }
+}
