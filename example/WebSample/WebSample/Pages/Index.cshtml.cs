02de3e3331a9b3a859ee4147e2306ee6eb82fa2c
kazenetu <rakudafactory@gmail.com>
2020-01-08 23:15:36 +0900

    ITestClassインスタンスプロパティと取得処理の追加実装

--- example/WebSample/WebSample/Pages/Index.cshtml.cs
+++ example/WebSample/WebSample/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Interface;
+using LoadAssembly;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -12,9 +14,12 @@ namespace WebSample.Pages
   {
     private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public ITestClass TestClass { private set; get; }
+
+    public IndexModel(ILogger<IndexModel> logger, IAssemblyLoader loader)
     {
       _logger = logger;
+      TestClass = loader.GetInterfaceInstance<ITestClass>();
     }
 
     public void OnGet()
