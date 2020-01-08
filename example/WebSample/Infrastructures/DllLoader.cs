5995080c65dc4329ab9ff5f6f9e6515386b8f548
kazenetu <rakudafactory@gmail.com>
2020-01-08 23:12:58 +0900

    Interfaceプロジェクトを追加

---
+++ example/WebSample/Infrastructures/DllLoader.cs
@@ -0,0 +1,27 @@
+﻿using LoadAssembly;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Infrastructures
+{
+  /// <summary>
+  /// コンストラクタ付きアセンブリ管理クラス
+  /// </summary>
+  public class DllLoader: AssemblyLoader
+  {
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public DllLoader()
+    {
+      // 読み込み対象のdllのフルパス設定
+      var thisPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+      var assemblyPath = Path.Combine(thisPath, "LoadTarget.dll");
+
+      Load(assemblyPath);
+    }
+  }
+}
